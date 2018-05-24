998990bc7797344a407bb7c5f28c79abf83264f8
Jan Funke <jfunke@iri.upc.edu>
2018-05-24 09:26:55 +0200

    Add watershed_from_affinities

--- lsd/fragments.py
+++ lsd/fragments.py
@@ -1,7 +1,8 @@
 import mahotas
 import numpy as np
 import logging
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage.morphology import distance_transform_edt
+from scipy.ndimage.filters import gaussian_filter, maximum_filter
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,34 @@ def watershed(lsds, sigma, return_distances=False, return_seeds=False):
     logger.info("Found %d fragments", n)
 
     ret = mahotas.cwatershed(boundary_distances, seeds).astype(np.uint64)
+
+    if return_distances or return_seeds:
+
+        ret = (ret,)
+        if return_distances:
+            ret = ret + (boundary_distances,)
+        if return_seeds:
+            ret = ret + (seeds.astype(np.uint64),)
+
+    return ret
+
+def watershed_from_affinities(affs, return_distances=False, return_seeds=False):
+    '''Extract initial fragments from local shape descriptors ``lsds`` using a
+    watershed transform. This assumes that the first three entries of
+    ``lsds`` for each voxel are vectors pointing towards the center.'''
+
+    boundary_mask = np.mean(affs, axis=0)>0.5
+    boundary_distances = distance_transform_edt(boundary_mask)
+    boundary_distances = (
+        boundary_distances.astype(np.float32)/
+        boundary_distances.max())
+    max_filtered = maximum_filter(boundary_distances, 10)
+    maxima = max_filtered==boundary_distances
+    seeds, n = mahotas.label(maxima)
+
+    logger.info("Found %d fragments", n)
+
+    ret = mahotas.cwatershed(1.0 - boundary_distances, seeds).astype(np.uint64)
 
     if return_distances or return_seeds:
 
