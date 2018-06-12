d4f28efc8b23148e3463ce31d86ae28f5a8cd72d
Jan Funke <jfunke@iri.upc.edu>
2018-06-12 10:57:02 +0200

    Increase test_parallel test size, store result

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -1,9 +1,10 @@
+from scipy.ndimage.filters import gaussian_filter, maximum_filter
+import gunpowder as gp
 import h5py
 import logging
 import lsd
 import mahotas
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter, maximum_filter
 
 # hack to avoid duplicate luigi logs
 from luigi.interface import setup_interface_logging
@@ -13,7 +14,8 @@ logging.basicConfig(level=logging.INFO)
 # logging.getLogger('lsd.persistence.sqlite_rag_provider').setLevel(logging.DEBUG)
 # logging.getLogger('lsd.agglomerate').setLevel(logging.DEBUG)
 
-size = (1, 200, 200)
+# context is 30 in xy, 0 in z; net size to work on is 10x500x500
+size = (10, 560, 560)
 
 def create_random_segmentation(seed):
 
@@ -33,12 +35,17 @@ if __name__ == "__main__":
     # intersect gt and fragments to get an oversegmentation
     fragments = gt + (fragments + 1)*gt.max()
 
-    lsd_extractor = lsd.LsdExtractor(sigma=(0.1, 10.0, 10.0))
-    predicted_lsds = lsd_extractor.get_descriptors(gt)
+    initial_fragments = np.array(fragments)
+
+    print("Computing target LSDs...")
+    lsd_extractor = lsd.LsdExtractor(sigma=(0.1, 10.0, 10.0), downsample=2)
+    target_lsds = lsd_extractor.get_descriptors(gt)
 
     rag_provider = lsd.persistence.SqliteRagProvider.from_fragments(
         fragments,
         'test_parallel_rag.db')
+
+    # PARALLEL AGGLOMERATION
 
     def block_done(block_roi):
 
@@ -47,13 +54,31 @@ if __name__ == "__main__":
 
         return all(done)
 
+    print("Starting parallel agglomeration...")
     agglomeration = lsd.ParallelLsdAgglomeration(
         rag_provider,
         fragments,
-        predicted_lsds,
+        target_lsds,
         lsd_extractor,
-        block_write_size=(1, 50, 50),
+        block_write_size=(10, 50, 50),
         block_done_function=block_done,
-        num_workers=1)
+        num_workers=10)
 
-    agglomeration.merge_until(0)
+    try:
+        agglomeration.merge_until(0)
+    except:
+        pass
+
+    # READ RESULT
+
+    segmentation = np.array(fragments)
+
+    rag = rag_provider[gp.Roi((0, 0, 0), size).to_slices()]
+    rag.contract_merged_nodes(segmentation)
+
+    with h5py.File('test_parallel.hdf', 'w') as f:
+
+        f['initial_fragments'] = initial_fragments
+        f['fragments'] = fragments
+        f['segmentation'] = segmentation
+        f['target_lsds'] = target_lsds
