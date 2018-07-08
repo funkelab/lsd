0c75dc2156b56ed5ab6bc7e49a5500f08510eef0
Jan Funke <funkej@janelia.hhmi.org>
2018-07-08 14:14:57 -0400

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,5 @@
 from setuptools import setup
 import subprocess
-try:
-    import base_string as string_types
-except ImportError:
-    string_types = str
-
-extras_require = {
-}
-
-
-dep_set = set()
-for value in extras_require.values():
-    if isinstance(value, string_types):
-        dep_set.add(value)
-    else:
-        dep_set.update(value)
-
-extras_require['full'] = list(dep_set)
-
-subprocess.call('pip install git+https://github.com/funkey/augment#egg=augment'.split())
 
 setup(
         name='lsd',
@@ -39,6 +20,5 @@ setup(
             "scikit-image",
             "requests",
             "gunpowder"
-        ],
-        extras_require=extras_require,
+        ]
 )
