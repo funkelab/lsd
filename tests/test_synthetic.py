117725376a2d493bd0566e7dafc7d843c88768b8
Jan Funke <funkej@janelia.hhmi.org>
2018-07-08 14:17:22 -0400

    Run with different noise levels in test_synthetic.py

--- tests/test_synthetic.py
+++ tests/test_synthetic.py
@@ -10,10 +10,6 @@ logging.getLogger('lsd.agglomerate').setLevel(logging.DEBUG)
 
 size = (1, 100, 100)
 
-# factor of pixel-wise uniformly sampled noise (in [-0.5, 0.5]) to add to
-# "prediction" LSDs
-noise_factor = 0
-
 def create_random_segmentation(seed):
 
     np.random.seed(seed)
@@ -27,46 +23,50 @@ def create_random_segmentation(seed):
 
 if __name__ == "__main__":
 
-    gt = create_random_segmentation(42)
-    fragments = create_random_segmentation(23)
-    # intersect gt and fragments to get an oversegmentation
-    fragments = gt + (fragments + 1)*gt.max()
+    # factor of pixel-wise uniformly sampled noise (in [-0.5, 0.5]) to add to
+    # "prediction" LSDs
+    for noise_factor in [0, 1, 2, 3]:
 
-    lsd_extractor = lsd.LsdExtractor(sigma=(10.0, 10.0, 10.0))
-    predicted_lsds = lsd_extractor.get_descriptors(gt)
+        gt = create_random_segmentation(42)
+        fragments = create_random_segmentation(23)
+        # intersect gt and fragments to get an oversegmentation
+        fragments = gt + (fragments + 1)*gt.max()
 
-    if noise_factor > 0:
-        noise = -0.5 + np.random.random(predicted_lsds.shape)
-        predicted_lsds += noise*noise_factor
-        predicted_lsds = predicted_lsds.clip(0, 1)
+        lsd_extractor = lsd.LsdExtractor(sigma=(10.0, 10.0, 10.0))
+        predicted_lsds = lsd_extractor.get_descriptors(gt)
 
-    agglomeration = lsd.LsdAgglomeration(
-        fragments,
-        predicted_lsds,
-        lsd_extractor)
+        if noise_factor > 0:
+            noise = -0.5 + np.random.random(predicted_lsds.shape)
+            predicted_lsds += noise*noise_factor
+            predicted_lsds = predicted_lsds.clip(0, 1)
 
-    # fragments and lsdss before merging
-    segmentations = [np.array(agglomeration.get_segmentation()[0])]
-    lsdss = [np.array(agglomeration.get_lsds()[:,0])]
+        agglomeration = lsd.LsdAgglomeration(
+            fragments,
+            predicted_lsds,
+            lsd_extractor)
 
-    while agglomeration.merge_until(0, max_merges=1) != 0:
-        segmentations.append(np.array(agglomeration.get_segmentation()[0]))
-        lsdss.append(np.array(agglomeration.get_lsds()[:,0]))
-    num_merges = len(segmentations)
+        # fragments and lsdss before merging
+        segmentations = [np.array(agglomeration.get_segmentation()[0])]
+        lsdss = [np.array(agglomeration.get_lsds()[:,0])]
 
-    print("Performed %d merges"%num_merges)
+        while agglomeration.merge_until(0, max_merges=1) != 0:
+            segmentations.append(np.array(agglomeration.get_segmentation()[0]))
+            lsdss.append(np.array(agglomeration.get_lsds()[:,0]))
+        num_merges = len(segmentations)
 
-    segmentations = np.array(segmentations)
-    lsdss = np.array(lsdss).transpose((1, 0, 2, 3))
-    gt = [gt[0]]*num_merges
-    predicted_lsds = np.array([predicted_lsds[:,0]]*num_merges).transpose((1, 0, 2, 3))
+        print("Performed %d merges"%num_merges)
 
-    diffs = np.sqrt(np.sum((predicted_lsds - lsdss)**2, axis=0))
-    diffs /= diffs.max()
+        segmentations = np.array(segmentations)
+        lsdss = np.array(lsdss).transpose((1, 0, 2, 3))
+        gt = [gt[0]]*num_merges
+        predicted_lsds = np.array([predicted_lsds[:,0]]*num_merges).transpose((1, 0, 2, 3))
 
-    with h5py.File('test_synthetic.hdf', 'w') as f:
-        f['volumes/gt'] = gt
-        f['volumes/predicted_lsds'] = predicted_lsds[0:3]
-        f['volumes/segmentations'] = segmentations
-        f['volumes/lsdss'] = lsdss[0:3]
-        f['volumes/diffs'] = diffs
+        diffs = np.sqrt(np.sum((predicted_lsds - lsdss)**2, axis=0))
+        diffs /= diffs.max()
+
+        with h5py.File('test_synthetic_noise=%d.hdf'%noise_factor, 'w') as f:
+            f['volumes/gt'] = gt
+            f['volumes/predicted_lsds'] = predicted_lsds[0:3]
+            f['volumes/segmentations'] = segmentations
+            f['volumes/lsdss'] = lsdss[0:3]
+            f['volumes/diffs'] = diffs
