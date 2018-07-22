98643337acd201946d7d90c2f0e3311ecf126d0f
Jan Funke <funkej@janelia.hhmi.org>
2018-07-22 13:08:31 -0400

    Rename parallel_agglomerate -> parallel_lsd_agglomerate

--- lsd/__init__.py
+++ lsd/__init__.py
@@ -1,6 +1,6 @@
 from agglomerate import LsdAgglomeration
 from local_shape_descriptor import LsdExtractor
-from parallel_agglomerate import ParallelLsdAgglomeration
+from parallel_lsd_agglomerate import ParallelLsdAgglomeration
 from parallel_fragments import parallel_watershed
 from rag import Rag
 from shared_rag_provider import SharedRagProvider, SubRag
