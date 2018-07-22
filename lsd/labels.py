0b9ac1dc5721882c1ddfe1d8c4a24caa7b3faa0f
Jan Funke <funkej@janelia.hhmi.org>
2018-07-22 17:36:11 -0400

    Add option to return backward map for labels.relabel

--- lsd/labels.py
+++ lsd/labels.py
@@ -7,10 +7,26 @@ def replace_values(array, old_values, new_values):
 
     return values_map[array]
 
-def relabel(array):
-    '''Relabel array, such that IDs are consecutive.'''
+def relabel(array, return_backwards_map=False):
+    '''Relabel array, such that IDs are consecutive. Excludes 0.'''
+
+    # get all labels except 0
     old_labels = np.unique(array)
+    old_labels = old_labels[old_labels != 0]
+
+    # shift previous labels to save some memory in replace_values
+    offset = old_labels.min()
+    shifted_array = array - offset
+    shifted_old_labels = old_labels - offset
+
     n = len(old_labels) + 1
     new_labels = np.arange(1, n, dtype=array.dtype)
 
-    return replace_values(array, old_labels, new_labels), n
+    replaced = replace_values(shifted_array, shifted_old_labels, new_labels)
+
+    if return_backwards_map:
+
+        backwards_map = np.insert(old_labels, 0, 0)
+        return replaced, n, backwards_map
+
+    return replaced, n
