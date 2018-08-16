c328b1bbc3c628bd5708ff0cc7a2746784c8617d
Jan Funke <funkej@janelia.hhmi.org>
2018-08-16 16:03:07 -0400

    Add option fragments_in_xy to watershed_from_affinities

--- lsd/fragments.py
+++ lsd/fragments.py
@@ -22,21 +22,55 @@ def watershed(lsds, sigma, return_seeds=False, return_distances=False):
 
     return ret
 
-def watershed_from_affinities(affs, return_seeds=False, return_distances=False):
+def watershed_from_affinities(affs, fragments_in_xy=False, return_seeds=False):
     '''Extract initial fragments from affinities using a watershed
-    transform.'''
+    transform. Returns the fragments and the maximal ID in it.'''
 
-    boundary_mask = np.mean(affs, axis=0)>0.5
-    boundary_distances = distance_transform_edt(boundary_mask)
+    if fragments_in_xy:
 
-    ret = watershed_from_boundary_distance(boundary_distances, return_seeds)
+        mean_affs = 0.5*(affs[1] + affs[2])
+        depth = mean_affs.shape[0]
 
-    if return_distances:
-        ret = ret + (boundary_distances,)
+        fragments = np.zeros(mean_affs.shape, dtype=np.uint64)
+        if return_seeds:
+            seeds = np.zeros(mean_affs.shape, dtype=np.uint64)
 
-    return ret
+        id_offset = 0
+        for z in range(depth):
 
-def watershed_from_boundary_distance(boundary_distances, return_seeds=False):
+            boundary_mask = mean_affs[z]>0.5
+            boundary_distances = distance_transform_edt(boundary_mask)
+
+            ret = watershed_from_boundary_distance(
+                boundary_distances,
+                return_seeds=return_seeds,
+                id_offset=id_offset)
+
+            fragments[z] = ret[0]
+            if return_seeds:
+                seeds[z] = ret[2]
+
+            id_offset = ret[1]
+
+        ret = (fragments, id_offset)
+        if return_seeds:
+            ret += (seeds,)
+
+        return ret
+
+    else:
+
+        boundary_mask = np.mean(affs, axis=0)>0.5
+        boundary_distances = distance_transform_edt(boundary_mask)
+
+        return watershed_from_boundary_distance(
+            boundary_distances,
+            return_seeds)
+
+def watershed_from_boundary_distance(
+        boundary_distances,
+        return_seeds=False,
+        id_offset=0):
 
     max_filtered = maximum_filter(boundary_distances, 10)
     maxima = max_filtered==boundary_distances
@@ -45,13 +79,15 @@ def watershed_from_boundary_distance(boundary_distances, return_seeds=False):
     logger.debug("Found %d fragments", n)
 
     if n == 0:
-        return np.zeros(affs.shape[1:], dtype=np.uint64), 0
+        return np.zeros(boundary_distances.shape, dtype=np.uint64), id_offset
+
+    seeds[seeds!=0] += id_offset
 
     fragments = mahotas.cwatershed(
         boundary_distances.max() - boundary_distances,
         seeds)
 
-    ret = (fragments.astype(np.uint64), n)
+    ret = (fragments.astype(np.uint64), n + id_offset)
     if return_seeds:
         ret = ret + (seeds.astype(np.uint64),)
 
