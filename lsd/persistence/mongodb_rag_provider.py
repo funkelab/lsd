7254c5833a63c06b2f99d7f4fde919ca691805d4
Jan Funke <funkej@janelia.hhmi.org>
2018-08-19 19:42:44 -0400

    Rewrite MongoDbRagProvider connection logic
    This allows to pickle the MongoDbRagProvider, as no open connection is
    kept between queries.

--- lsd/persistence/mongodb_rag_provider.py
+++ lsd/persistence/mongodb_rag_provider.py
@@ -102,45 +102,76 @@ class MongoDbRagProvider(SharedRagProvider):
     '''A shared region adjacency graph stored in an SQLite file.
     '''
 
-    def __init__(self, db_name, mode):
+    def __init__(self, db_name, host=None, mode='r+'):
 
         self.db_name = db_name
+        self.host = host
         self.mode = mode
-        self.client = MongoClient()
+        self.client = None
+        self.database = None
+        self.nodes = None
+        self.edges = None
 
-        if mode == 'w':
-            self.client.drop_database(db_name)
+        try:
 
-        self.database = self.client[db_name]
+            self.__connect()
+
+            if mode == 'w':
+                self.client.drop_database(db_name)
+
+            if self.db_name not in self.client.list_database_names():
+                self.__setup_db()
+
+        finally:
+
+            self.__disconnect()
+
+    def __connect(self):
+
+        self.client = MongoClient(self.host)
+
+    def __open_db(self):
+
+        self.database = self.client[self.db_name]
         self.nodes = self.database['nodes']
         self.edges = self.database['edges']
 
-        if mode == 'w':
+    def __disconnect(self):
 
-            self.nodes.create_index(
-                [
-                    ('center_z', ASCENDING),
-                    ('center_y', ASCENDING),
-                    ('center_x', ASCENDING)
-                ],
-                name='position')
+        self.nodes = None
+        self.edges = None
+        self.database = None
+        self.client.close()
+        self.client = None
 
-            self.nodes.create_index(
-                [
-                    ('id', ASCENDING)
-                ],
-                name='id',
-                unique=True)
+    def __setup_db(self):
 
-            self.edges.create_index(
-                [
-                    ('u', ASCENDING),
-                    ('v', ASCENDING)
-                ],
-                name='incident',
-                unique=True)
+        self.__open_db()
 
-    def read_nodes(self, roi):
+        self.nodes.create_index(
+            [
+                ('center_z', ASCENDING),
+                ('center_y', ASCENDING),
+                ('center_x', ASCENDING)
+            ],
+            name='position')
+
+        self.nodes.create_index(
+            [
+                ('id', ASCENDING)
+            ],
+            name='id',
+            unique=True)
+
+        self.edges.create_index(
+            [
+                ('u', ASCENDING),
+                ('v', ASCENDING)
+            ],
+            name='incident',
+            unique=True)
+
+    def __read_nodes(self, roi):
         '''Return a list of nodes within roi.
         '''
 
@@ -160,32 +191,43 @@ class MongoDbRagProvider(SharedRagProvider):
 
     def __getitem__(self, roi):
 
-        assert len(slices) == 3, "Sorry, MongoDbRagProvider backend does only 3D"
+        assert roi.dims() == 3, "Sorry, MongoDbRagProvider backend does only 3D"
 
-        # get all nodes within roi
-        nodes = self.read_nodes(roi)
-        logger.debug("found %d nodes", len(nodes))
+        try:
 
-        # create a list of nodes and their attributes
-        node_list = [
-            (n['id'], self.__remove_keys(n, ['id']))
-            for node in nodes
-        ]
-        logger.debug("read nodes: %s", node_list)
+            self.__connect()
+            self.__open_db()
 
-        # get all edges that have their u in the selected nodes
-        node_ids = list([ n['id'] for n in nodes])
-        edges = self.edges.find(
-            {
-                'source': { '$in': node_ids }
-            })
+            # get all nodes within roi
+            nodes = self.__read_nodes(roi)
 
-        # create a list of edges and their attributes
-        edge_list = [
-            (e['u'], e['v'], self.__remove_keys(e, ['u', 'v']))
-            for e in edges
-        ]
-        logger.debug("read edges: %s", edge_list)
+            # create a list of nodes and their attributes
+            node_list = [
+                (n['id'], self.__remove_keys(n, ['id']))
+                for n in nodes
+            ]
+            logger.debug("found %d nodes", len(node_list))
+            logger.debug("read nodes: %s", node_list)
+
+            # get all edges that have their u in the selected nodes
+            node_ids = list([ node[0] for node in node_list])
+            logger.debug("looking for edges with u in %s", node_ids)
+            edges = self.edges.find(
+                {
+                    'u': { '$in': node_ids }
+                })
+
+            # create a list of edges and their attributes
+            edge_list = [
+                (e['u'], e['v'], self.__remove_keys(e, ['u', 'v']))
+                for e in edges
+            ]
+            logger.debug("found %d edges", len(edge_list))
+            logger.debug("read edges: %s", edge_list)
+
+        finally:
+
+            self.__disconnect()
 
         # create the sub-RAG
         graph = MongoDbSubRag(self.db_name, self.host, self.mode)
