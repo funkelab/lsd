Fix voxel scaling in derivatives

lsd/local_shape_descriptor.py
@@ -198,17 +198,17 @@ class LsdExtractor(object):
         k_z, k_y, k_x = self.__generate_kernels(voxel_size)
 
         # first derivatives
-        d_z = convolve(soft_mask, k_z, mode='constant')
-        d_y = convolve(soft_mask, k_y, mode='constant')
-        d_x = convolve(soft_mask, k_x, mode='constant')
+        d_z = convolve(soft_mask, k_z, mode='constant')/voxel_size[0]
+        d_y = convolve(soft_mask, k_y, mode='constant')/voxel_size[1]
+        d_x = convolve(soft_mask, k_x, mode='constant')/voxel_size[2]
 
         # second derivatives
-        d_zz = convolve(d_z, k_z, mode='constant')
-        d_yy = convolve(d_y, k_y, mode='constant')
-        d_xx = convolve(d_x, k_x, mode='constant')
-        d_zy = convolve(d_z, k_y, mode='constant')
-        d_zx = convolve(d_z, k_x, mode='constant')
-        d_yx = convolve(d_y, k_x, mode='constant')
+        d_zz = convolve(d_z, k_z, mode='constant')/voxel_size[0]
+        d_yy = convolve(d_y, k_y, mode='constant')/voxel_size[1]
+        d_xx = convolve(d_x, k_x, mode='constant')/voxel_size[2]
+        d_zy = convolve(d_z, k_y, mode='constant')/voxel_size[1]
+        d_zx = convolve(d_z, k_x, mode='constant')/voxel_size[2]
+        d_yx = convolve(d_y, k_x, mode='constant')/voxel_size[2]
 
         # Max normalization for each instance:
         soft_mask /= np.max(np.abs(soft_mask))
@@ -244,16 +244,16 @@ class LsdExtractor(object):
         """
 
         k_z = np.zeros((3, 1, 1), dtype=np.float32)
-        k_z[0, 0, 0] = voxel_size[2]
-        k_z[2, 0, 0] = -voxel_size[2]
+        k_z[0, 0, 0] = voxel_size[0]
+        k_z[2, 0, 0] = -voxel_size[0]
 
         k_y = np.zeros((1, 3, 1), dtype=np.float32)
         k_y[0, 0, 0] = voxel_size[1]
         k_y[0, 2, 0] = -voxel_size[1]
 
         k_x = np.zeros((1, 1, 3), dtype=np.float32)
-        k_x[0, 0, 0] = voxel_size[0]
-        k_x[0, 0, 2] = -voxel_size[0]
+        k_x[0, 0, 0] = voxel_size[2]
+        k_x[0, 0, 2] = -voxel_size[2]
 
         return k_z, k_y, k_x
 