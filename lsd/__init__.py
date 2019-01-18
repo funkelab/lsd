be4c965567cd392e4f4cb0dc6ab32831baeb4240
Arlo Sheridan <sheridana@janelia.hhmi.org>
2019-01-18 11:09:12 -0500

    Expose agglomerate_in_block in public API

--- lsd/__init__.py
+++ lsd/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from .agglomerate import LsdAgglomeration
 from .local_shape_descriptor import LsdExtractor
 from .merge_tree import MergeTree
-from .parallel_aff_agglomerate import parallel_aff_agglomerate
+from .parallel_aff_agglomerate import parallel_aff_agglomerate, agglomerate_in_block
 from .parallel_fragments import parallel_watershed, watershed_in_block
 from .parallel_lsd_agglomerate import parallel_lsd_agglomerate
 from .rag import Rag
