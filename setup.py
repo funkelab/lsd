6901d42610cf8fad145b63e3515dbded65d92cea
Nils Eckstein <ecksteinn@ecksteinn-ws1.int.janelia.org>
2019-03-06 15:39:57 -0500

    Add numpy include directory

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from Cython.Distutils import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
+import numpy as np
 
 setup(
         name='lsd',
@@ -19,6 +20,7 @@ setup(
                 'lsd.replace_values',
                 sources=['lsd/replace_values.pyx'],
                 extra_compile_args=['-O3'],
+		include_dirs=[np.get_include()],
                 language='c++')
         ],
         cmdclass={'build_ext': build_ext}
