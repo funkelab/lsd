d77bf0b2f774baf9127a6e9fbc72bc1a2eb459a3
sheridana <sheridana@janelia.hhmi.org>
2022-10-28 15:10:47 -0700

    Remove mahotas from setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
         name='lsds',
-        version='0.1.1',
+        version='0.1.2',
         description='Local Shape Descriptors.',
         long_description=long_description,
         long_description_content_type='text/markdown',
@@ -31,7 +31,6 @@ setup(
         ],
         cmdclass={'build_ext': build_ext},
         install_requires=[
-            "mahotas",
             "numpy",
             "scipy",
             "h5py",
